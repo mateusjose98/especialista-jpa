edbc69710317064843b266559bb119207d762cf6
mateus <josemateus.ufma@gmail.com>
2021-09-27 21:23:11 -0300

    Atualizando um objeto

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -9,52 +9,69 @@ import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.model.Produto;
 
 public class OperacoesComTransacaoTest extends EntityManagerTest {
-	
-	
+
 	@Test
-	public void removerObjeto() {
+	public void atualizarObjeto() {
 		
-		Produto entity = entityManager.find(Produto.class, 3);
-		entity.setId(3);
+		Produto produto = new Produto();
 		
+		produto.setId(1);
+		produto.setNome("Chinelo de dedo");
+		produto.setDescricao("Sandalia de dedo");
+		produto.setPreco(new BigDecimal(10.00));
+
+
 		entityManager.getTransaction().begin();
-		
-		
-		entityManager.remove(entity);
-		
-		
+		entityManager.merge(produto);
 		entityManager.getTransaction().commit();
 		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);
+		Assert.assertEquals("Chinelo de dedo", produtoVerificacao.getNome());
+
+	}
+
+	@Test
+	public void removerObjeto() {
+
+		Produto entity = entityManager.find(Produto.class, 3);
+		entity.setId(3);
+
+		entityManager.getTransaction().begin();
+
+		entityManager.remove(entity);
+
+		entityManager.getTransaction().commit();
+
 		Produto produtoVerificacao = entityManager.find(Produto.class, 3);
 		Assert.assertNull(produtoVerificacao);
-		
-		
-		
-		
+
 	}
-	
+
 	@Test
 	public void inserirOPrimeiroObjeto() {
-		
+
 		Produto produto = new Produto();
 		produto.setId(2);
 		produto.setDescricao("A melhor camera do mundo!!!!");
 		produto.setNome("Canon");
 		produto.setPreco(new BigDecimal(1050.0));
-		
-		// o perist funcionaria antes também, pois deixa o objeto aguardando em memória até que uma transação seja feita
-		
+
+		// o perist funcionaria antes também, pois deixa o objeto aguardando em memória
+		// até que uma transação seja feita
+
 		entityManager.getTransaction().begin();
-		
+
 		entityManager.persist(produto);
-		
+
 		entityManager.getTransaction().commit();
-		
+
 		entityManager.clear();
-		
+
 		Produto produtoVerificacao = entityManager.find(Produto.class, 2);
 		Assert.assertNotNull(produtoVerificacao);
-		
 
 	}
 
